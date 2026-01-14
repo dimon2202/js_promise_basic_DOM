e206741eed11ca7bd3239c5a64908f68cf0dfeab
Dima Sidenko <sidenkodima2202@gmail.com>
2026-01-14 09:10:22 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,10 +26,18 @@ const handler = (text, styles) => {
   document.body.appendChild(div);
 };
 
-promise1().then(() => {
-  handler('Promise was resolved!', ['message']);
-});
+promise1()
+  .then(() => {
+    handler('Promise was resolved!', ['message']);
+  })
+  .catch(() => {
+    handler('Promise was rejected!', ['message', 'error-message']);
+  });
 
-promise2().catch(() => {
-  handler('Promise was rejected!', ['message', 'error-message']);
-});
+promise2()
+  .then(() => {
+    handler('Promise was resolved!', ['message']);
+  })
+  .catch(() => {
+    handler('Promise was rejected!', ['message', 'error-message']);
+  });
